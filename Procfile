d2b26916e0ec3473db438be5d223d484533026f0
idbabis <idbabis@gmail.com>
2022-07-16 14:50:06 -0300

    add tudo que foi feito em aula

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: npm install && node server.js
+
--- src/app.js
+++ src/app.js
@@ -1,44 +1,44 @@
-const express = require("express")
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const compression = require('compression');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const compression = require("compression");
 
-const index = require("./routes/index")
-const candidatas = require("./routes/candidatasRouter")
-const empresas = require("./routes/empresaRouter")
-const vagas = require("./routes/vagasRouter")
+const index = require("./routes/index");
+const candidatas = require("./routes/candidatasRouter");
+const empresas = require("./routes/empresaRouter");
+const vagas = require("./routes/vagasRouter");
 const dataBase = require("./model/database");
+const swaggerFile = require("../swagger/swagger_output.json");
+const swaggerUi = require("swagger-ui-express");
 
+require("dotenv-safe").config();
 
-require('dotenv-safe').config()
+dataBase.connect();
 
-dataBase.connect()
+const app = express();
 
-const app = express()
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-
-app.use(morgan('dev'))
-app.use(helmet())
-app.use(cors())
-app.use(compression())
 
 app.use(function (request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*")
+  response.header("Access-Control-Allow-Origin", "*");
   response.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  next()
-})
+  );
+  next();
+});
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/", index);
 app.use("/candidatas", candidatas);
 app.use("/empresas", empresas);
-app.use("/vagas", vagas)
+app.use("/vagas", vagas);
 
-
-module.exports = app
-
+module.exports = app;
